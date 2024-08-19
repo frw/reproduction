08f11c5f53dc97c59ef815ba727d658b0a88a674
Frederick Widjaja <widjaja.frederick@gmail.com>
2024-08-19 18:13:10 +0700

    add failing example

--- src/example.test.ts
+++ src/example.test.ts
@@ -1,8 +1,43 @@
-import { Entity, MikroORM, PrimaryKey, Property } from '@mikro-orm/sqlite';
+import {
+  Embeddable,
+  Embedded,
+  Entity,
+  LoadStrategy,
+  ManyToOne,
+  MikroORM,
+  PrimaryKey,
+  Property,
+} from "@mikro-orm/sqlite";
+
+@Entity()
+class Organization {
+  @PrimaryKey()
+  id!: number;
+
+  @Property()
+  name: string;
+
+  @Property({ lazy: true })
+  tag: string;
+
+  constructor(name: string, tag: string) {
+    this.name = name;
+    this.tag = tag;
+  }
+}
+
+@Embeddable()
+class Properties {
+  @ManyToOne({ entity: () => Organization })
+  organization: Organization;
+
+  constructor(organization: Organization) {
+    this.organization = organization;
+  }
+}
 
 @Entity()
 class User {
-
   @PrimaryKey()
   id!: number;
 
@@ -12,20 +47,23 @@ class User {
   @Property({ unique: true })
   email: string;
 
-  constructor(name: string, email: string) {
+  @Embedded()
+  properties: Properties;
+
+  constructor(name: string, email: string, properties: Properties) {
     this.name = name;
     this.email = email;
+    this.properties = properties;
   }
-
 }
 
 let orm: MikroORM;
 
 beforeAll(async () => {
   orm = await MikroORM.init({
-    dbName: ':memory:',
-    entities: [User],
-    debug: ['query', 'query-params'],
+    dbName: ":memory:",
+    entities: [User, Properties, Organization],
+    debug: ["query", "query-params"],
     allowGlobalContext: true, // only for testing
   });
   await orm.schema.refreshDatabase();
@@ -35,17 +73,24 @@ afterAll(async () => {
   await orm.close(true);
 });
 
-test('basic CRUD example', async () => {
-  orm.em.create(User, { name: 'Foo', email: 'foo' });
+test("basic CRUD example", async () => {
+  const organization = orm.em.create(Organization, { name: "Bar", tag: "bar" });
+  orm.em.create(User, { name: "Foo", email: "foo", properties: new Properties(organization) });
   await orm.em.flush();
   orm.em.clear();
 
-  const user = await orm.em.findOneOrFail(User, { email: 'foo' });
-  expect(user.name).toBe('Foo');
-  user.name = 'Bar';
-  orm.em.remove(user);
-  await orm.em.flush();
+  const user1 = await orm.em.findOneOrFail(
+    User,
+    { email: "foo" },
+    { fields: ["*", "properties.organization.tag"] }
+  );
+  expect(user1.properties.organization.tag).toBe("bar");
+  orm.em.clear();
 
-  const count = await orm.em.count(User, { email: 'foo' });
-  expect(count).toBe(0);
+  const user2 = await orm.em.findOneOrFail(
+    User,
+    { email: "foo" },
+    { populate: ["properties.organization.tag"] }
+  );
+  expect(user2.properties.organization.tag).toBe("bar");
 });
